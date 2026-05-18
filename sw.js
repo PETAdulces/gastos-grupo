f87b3f91885aab4ec93ae13ffc94c1ff26ecdfa3
PETAdulces <mrfberet@gmail.com>
2026-05-18 19:50:16 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,18 @@
+const CACHE = 'cuentas-v1';
+const FILES = ['/'];
+
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function(cache) {
+      return cache.addAll(FILES);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    fetch(e.request).catch(function() {
+      return caches.match(e.request);
+    })
+  );
+});
